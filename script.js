8352d471fedf192b267c18945ff6c9b29125c0ac
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-11 22:52:33 -0500

    Needs some more work

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-const charRange=document.getElementById("charRange")
-const charNumber=document.getElementById("charNumber")
+const charRange = document.getElementById("charRange")
+const charNumber = document.getElementById("charNumber")
+const passGenForm = document.getElementById("passGenForm")
+const caseIncldElem = document.getElementById("upper")
+const numIncldElem = document.getElementById("nums")
+const symIncldElem = document.getElementById("syms")
 
-// Defining event listeners
 
-charRange.addEventListener("input",syncCharAmount)
-charNumber.addEventListener("input",syncCharAmount)
+const UPPERCASE_CHAR_CODES = arrayRange(65, 90)
+const LOWERCASE_CHAR_CODES = arrayRange(97, 122)
+const NUMBER_CHAR_CODES = arrayRange(48, 57)
+const SYMBOL_CHAR_CODES = arrayRange(33, 47).concat(arrayRange(58, 126))
 
-function syncCharAmount(e){
-    const value=e.target.value;
-    charRange.value=value;
-    charNumber.value=value;
+
+charRange.addEventListener("input", syncCharAmount)
+charNumber.addEventListener("input", syncCharAmount)
+
+
+passGenForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const charAmount = charNumber.value;
+    const caseIncld = caseIncldElem.checked
+    const numIncld = numIncldElem.checked
+    const symIncld = symIncldElem.checked
+
+
+    const passWord = generatePassword(charAmount, caseIncld, numIncld, symIncld)
+})
+
+function generatePassword(a, b, c, d) {
+    console.log(LOWERCASE_CHAR_CODES);
+
+
+
 }
 
+
+function arrayRange(low, high) {
+    const array = [];
+    for (let i = low; i < high.length; i++) {
+        array.push(i)
+    }
+    return array
+}
+
+
+
+function syncCharAmount(e) {
+    const value = e.target.value;
+    charRange.value = value;
+    charNumber.value = value;
+}
