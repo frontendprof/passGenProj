132b5f435fc143f8d61176f0dec40a725649be55
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-14 03:25:11 -0500

    Buggy code

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const upper = document.getElementById("upper");
 const nmbrs = document.getElementById("nums");
 const symbls = document.getElementById("syms")
 
+const passDisplay = document.querySelector(".password-display");
+
 
 charNumber.addEventListener("input", syncInputAmount);
 charRange.addEventListener("input", syncInputAmount);
@@ -24,18 +26,26 @@ passGenForm.addEventListener("submit", (e) => {
   const numberIncld = nmbrs.checked;
   const symblIncld = symbls.checked;
   const password = genPassword(numberChars, upperIncld, numberIncld, symblIncld);
+  passDisplay.innerText = password;
 });
 
 function genPassword(numberChars, upperIncld, numberIncld, symblIncld) {
-  console.log(UPPER_CASE_CODES);
 
+  let charCodes = LOWER_CASE_CODES;
+  if (upperIncld) charCodes = charCodes.concat(UPPER_CASE_CODES);
+  if (numberIncld) charCodes = charCodes.concat(NUMBER_CASE_CODES);
+  if (symblIncld) charCodes = charCodes.concat(SYMBOL_CASE_CODES);
+
+  const passCharacters = [];
+  for (let i = 0; i < charNumber.length; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
+    passCharacters.push(String.fromCharCode(characterCode));
+
+  }
+  return passCharacters.join('');
 
 }
 
-const UPPER_CASE_CODES = lowToHigh(65, 90);
-const LOWER_CASE_CODES = lowToHigh(97, 122);
-const NUMBER_CASE_CODES = lowToHigh(48, 57);
-const SYMBOL_CASE_CODES = lowToHigh(33, 47).concat(lowToHigh(58, 64)).concat(lowToHigh(91, 95)).concat(lowToHigh(123, 126))
 
 function lowToHigh(low, high) {
   const array = [];
@@ -44,3 +54,8 @@ function lowToHigh(low, high) {
   }
   return array;
 }
+
+const UPPER_CASE_CODES = lowToHigh(65, 90);
+const LOWER_CASE_CODES = lowToHigh(97, 122);
+const NUMBER_CASE_CODES = lowToHigh(48, 57);
+const SYMBOL_CASE_CODES = lowToHigh(33, 47).concat(lowToHigh(58, 64)).concat(lowToHigh(91, 95)).concat(lowToHigh(123, 126))
